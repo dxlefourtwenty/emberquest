d7c9033305d14ce7d25cb823553149e3716aa343
Dale Peligro <daledoesminecraft2013@gmail.com>
2025-09-27 14:00:50 -0700

    added most visual components

--- lib/ember_quest.dart
+++ lib/ember_quest.dart
@@ -2,9 +2,18 @@ import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 
 import 'actors/ember.dart';
+import 'actors/water_enemy.dart';
+import 'managers/segment_manager.dart';
+import 'objects/ground_block.dart';
+import 'objects/platform_block.dart';
+import 'objects/star.dart';
+import 'package:flutter/material.dart';
 
 class EmberQuestGame extends FlameGame {
   late EmberPlayer _ember;
+  double objectSpeed = 0.0;
+  late double lastBlockXPosition = 0.0;
+  late UniqueKey lastBlockKey;
 
   @override
   Future<void> onLoad() async {
@@ -22,10 +31,60 @@ class EmberQuestGame extends FlameGame {
     // of the `CameraComponent`s viewfinder (where the camera is looking)
     // is in the top left corner, that's why we set the anchor here.
     camera.viewfinder.anchor = Anchor.topLeft;
+    initializeGame();
+  }
+
+  void loadGameSegments(int segmentIndex, double xPositionOffset) {
+    for (final block in segments[segmentIndex]) {
+      switch (block.blockType) {
+        case GroundBlock:
+          world.add(
+            GroundBlock(
+              gridPosition: block.gridPosition,
+              xOffset: xPositionOffset,
+            ),
+          );
+        case PlatformBlock:
+          add(PlatformBlock(
+            gridPosition: block.gridPosition,
+            xOffset: xPositionOffset,
+          ));
+        case Star:
+          world.add(
+            Star(
+              gridPosition: block.gridPosition,
+              xOffset: xPositionOffset,
+            ),
+          );
+        case WaterEnemy:
+          world.add(
+            WaterEnemy(
+              gridPosition: block.gridPosition,
+              xOffset: xPositionOffset,
+            ),
+          );
+      }
+    }
+  }
+
+  void initializeGame() {
+    // Assume that size.x < 3200
+    final segmentsToLoad = (size.x / 640).ceil();
+    segmentsToLoad.clamp(0, segments.length);
+
+    for (var i = 0; i <= segmentsToLoad; i++) {
+      loadGameSegments(i, (640 * i).toDouble());
+    }
 
     _ember = EmberPlayer(
       position: Vector2(128, canvasSize.y - 70),
     );
     world.add(_ember);
   }
+
+  @override
+  Color backgroundColor() {
+    return const Color.fromARGB(255, 173, 223, 247);
+  }
+
 }
